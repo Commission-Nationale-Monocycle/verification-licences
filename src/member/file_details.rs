b8fb3eac13d8643422539192fbf2e894cd7fc5be
Maxence Cornaton <cirque.m@hotmail.fr>
2025-02-25 10:23:16 +0100

    Test `FileDetails`

--- src/member/file_details.rs
+++ src/member/file_details.rs
@@ -13,3 +13,20 @@ impl FileDetails {
         Self { update_date, filename }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::ffi::OsString;
+    use chrono::NaiveDate;
+    use crate::member::file_details::FileDetails;
+
+    #[test]
+    fn test_new_file_details() {
+        let date = NaiveDate::from_ymd_opt(2025, 1, 2).unwrap();
+        let filename = OsString::from("file");
+        let details = FileDetails::new(date, filename.clone());
+
+        assert_eq!(&date, details.update_date());
+        assert_eq!(&filename, details.filename());
+    }
+}
