4b61820fce872c125c817ff5216fc9f02cd790cc
Maxence Cornaton <22578053+maxence-cornaton@users.noreply.github.com>
2025-02-27 13:09:36 +0100

    Serve static files under `/public/static` (#9)
    * Serve static files under /public/static
    
    * Add favicon
    
    ---------
    
    Co-authored-by: Maxence Cornaton <maxence.cornaton+git@proton.me>

--- src/web/frontend/server.rs
+++ src/web/frontend/server.rs
@@ -1,4 +1,5 @@
 use rocket::{Build, Rocket};
+use rocket::fs::FileServer;
 use rocket_dyn_templates::Template;
 use crate::web::server::Server;
 use crate::web::frontend::frontend_controller;
@@ -19,6 +20,7 @@ impl Server for FrontendServer {
             frontend_controller::index,
             frontend_controller::hello
         ])
+            .mount("/", FileServer::from("./public/static"))
             .register("/", catchers![frontend_controller::not_found])
             .attach(Template::fairing())
     }
