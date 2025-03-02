b78be9dfbbba4ce086492e5b7d0e31275500fcc4
Maxence Cornaton <22578053+maxence-cornaton@users.noreply.github.com>
2025-03-02 19:27:06 +0100

    Fix WASM automatic build (#26)
    * Fix WASM automatic build
    
    * Disable asserts
    
    * Completely remove asserts
    
    ---------
    
    Co-authored-by: Maxence Cornaton <maxence.cornaton+git@proton.me>

--- build.rs
+++ build.rs
@@ -1,12 +1,27 @@
+use std::fs;
 use std::process::Command;
 
 fn main() {
     println!("cargo::rerun-if-changed=src/web/frontend/wasm/src");
+    let compilation_path = "target-wasm";
+    let pkg_path = "public/static/pkg";
+    delete_entity(compilation_path);
+    delete_entity(pkg_path);
     let profile = std::env::var("PROFILE").unwrap();
+    let profile = profile.as_str();
+    let wasm_file_path = &format!(
+        "{compilation_path}/wasm32-unknown-unknown/{profile}/wasm_verification_licences.wasm"
+    );
+    compile_wasm(compilation_path, profile);
+    generate_bindings(wasm_file_path, pkg_path);
+}
+
+fn compile_wasm(compilation_path: &str, profile: &str) {
+    let target_dir = format!("--target-dir={compilation_path}");
     let mut build_args = vec![
         "build",
         "--target=wasm32-unknown-unknown",
-        "--target-dir=target-wasm",
+        target_dir.as_str(),
         "--manifest-path=./src/web/frontend/wasm/Cargo.toml",
     ];
     if profile == "release" {
@@ -16,16 +31,25 @@ fn main() {
         .args(build_args)
         .output()
         .expect("Failed to compile frontend.");
+}
 
-    let wasm_file_path =
-        format!("target-wasm/wasm32-unknown-unknown/{profile}/wasm_verification_licences.wasm");
-    let wasm_bindgen_args = [
-        "--target=web",
-        "--out-dir=public/static/pkg",
-        wasm_file_path.as_str(),
-    ];
+/// Generate JS & TS bindings
+fn generate_bindings(wasm_file_path: &str, pkg_path: &str) {
+    let out_dir_param = format!("--out-dir={pkg_path}");
+    let wasm_bindgen_args = ["--target=web", out_dir_param.as_str(), wasm_file_path];
     Command::new("wasm-bindgen")
         .args(wasm_bindgen_args)
         .output()
         .expect("Failed to generate WASM wrappers.");
+}
+
+fn delete_entity(compilation_path: &str) {
+    match fs::metadata(compilation_path) {
+        Ok(metadata) if metadata.is_dir() => fs::remove_dir_all(compilation_path)
+            .expect(&format!("Couldn't delete {compilation_path}")),
+        Ok(_) => {
+            fs::remove_file(compilation_path).expect(&format!("Couldn't delete {compilation_path}"))
+        }
+        Err(_) => {}
+    }
 }
