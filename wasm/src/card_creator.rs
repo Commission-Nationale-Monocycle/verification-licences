fe721c54f6b0bcf21789cea06013883c6d82e2b1
Maxence Cornaton <22578053+maxence-cornaton@users.noreply.github.com>
2025-03-05 14:46:01 +0100

    Add test harness to wasm crate (#36)
    * Add test harness to wasm crate
    
    * Rearrange `utils.rs` file
    
    * Test `utils.rs`
    
    * Test card_creator.rs
    
    ---------
    
    Co-authored-by: Maxence Cornaton <maxence.cornaton+git@proton.me>

--- wasm/src/card_creator.rs
+++ wasm/src/card_creator.rs
@@ -1,7 +1,7 @@
-use web_sys::{Document, Element};
+use crate::utils::{create_element, create_element_with_class, create_element_with_classes};
 use dto::member_to_check::MemberToCheck;
 use dto::membership::Membership;
-use crate::utils::{create_element, create_element_with_class, create_element_with_classes};
+use web_sys::{Document, Element};
 
 pub trait CardCreator {
     fn create_card(&self, document: &Document) -> Element;
@@ -75,4 +75,60 @@ impl CardCreator for MemberToCheck {
 
         container
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use chrono::Utc;
+    use wasm_bindgen_test::wasm_bindgen_test;
+
+    // region Membership
+    #[wasm_bindgen_test]
+    fn should_create_card_for_known_membership() {
+        let membership = Membership::new(
+            "Doe".to_owned(),
+            "Jon".to_owned(),
+            "M".to_owned(),
+            None,
+            None,
+            "123456".to_owned(),
+            "email@address.org".to_owned(),
+            true,
+            Utc::now().date_naive(),
+            false,
+            "club".to_owned(),
+            "structure_code".to_owned(),
+        );
+        let document = Document::new().unwrap();
+
+        let element = Membership::create_card_from_optional(&Some(&membership), &document);
+        let inner_html = element.inner_html();
+        let expected_inner_html = "<div class=\"font-semibold\">Membre associé au numéro d'adhésion fourni</div><div>Nom : Doe</div><div>Prénom : Jon</div><div>Fin de l'adhésion : 05/03/2025</div><div>Adresse mail : email@address.org</div>";
+        assert_eq!(expected_inner_html, inner_html);
+    }
+
+    #[wasm_bindgen_test]
+    fn should_create_card_for_unknown_membership() {
+        let document = Document::new().unwrap();
+
+        let element = Membership::create_card_from_optional(&None, &document);
+        let inner_html = element.inner_html();
+        let expected_inner_html = "<div class=\"font-semibold\">Aucune adhésion trouvée</div>";
+        assert_eq!(expected_inner_html, inner_html);
+    }
+    // endregion
+
+    // region MemberToCheck
+    #[wasm_bindgen_test]
+    fn should_create_card_for_member_to_check() {
+        let member_to_check = MemberToCheck::new("123456".to_owned(), "Doe".to_owned(), "Jon".to_owned());
+        let document = Document::new().unwrap();
+
+        let element = member_to_check.create_card(&document);
+        let inner_html = element.inner_html();
+        let expected_inner_html = "<div class=\"font-semibold\">Membre à vérifier</div><div>Numéro d'adhésion : 123456</div><div>Nom : Doe</div><div>Prénom : Jon</div>";
+        assert_eq!(expected_inner_html, inner_html);
+    }
+    // endregion
 }
--- wasm/src/utils.rs
+++ wasm/src/utils.rs
@@ -12,6 +12,34 @@ pub fn set_panic_hook() {
     console_error_panic_hook::set_once();
 }
 
+// region Get elements
+pub fn get_window() -> web_sys::Window {
+    web_sys::window().expect("no global `window` exists")
+}
+
+pub fn get_document() -> Document {
+    let window = get_window();
+    window.document().expect("should have a document on window")
+}
+
+pub fn get_element_by_id(document: &Document, id: &str) -> Element {
+    document
+        .get_element_by_id(id)
+        .unwrap_or_else(|| panic!("`{id}` element does not exist"))
+}
+
+pub fn get_element_by_id_dyn<T: JsCast>(document: &Document, id: &str) -> T {
+    get_element_by_id(document, id).dyn_into().unwrap()
+}
+
+pub fn get_value_from_input(document: &Document, id: &str) -> String {
+    get_element_by_id(document, id)
+        .get_attribute("value")
+        .unwrap_or_else(|| panic!("`{id}` input does not contain text"))
+}
+// endregion
+
+// region Create elements
 pub fn create_element(
     document: &Document,
     name: &str,
@@ -58,32 +86,9 @@ pub fn create_element_with_classes(
     new_element.set_class_name(&classes.join(" "));
     new_element
 }
+// endregion
 
-pub fn get_window() -> web_sys::Window {
-    web_sys::window().expect("no global `window` exists")
-}
-
-pub fn get_document() -> Document {
-    let window = get_window();
-    window.document().expect("should have a document on window")
-}
-
-pub fn get_element_by_id(document: &Document, id: &str) -> Element {
-    document
-        .get_element_by_id(id)
-        .unwrap_or_else(|| panic!("`{id}` element does not exist"))
-}
-
-pub fn get_element_by_id_dyn<T: JsCast>(document: &Document, id: &str) -> T {
-    get_element_by_id(document, id).dyn_into().unwrap()
-}
-
-pub fn get_value_from_input(document: &Document, id: &str) -> String {
-    get_element_by_id(document, id)
-        .get_attribute("value")
-        .unwrap_or_else(|| panic!("`{id}` input does not contain text"))
-}
-
+// region Manipulate existing elements
 pub fn append_child(container: &Element, child: &Element) {
     container.append_child(child).expect("can't append child");
 }
@@ -102,4 +107,222 @@ pub fn remove_attribute(element: &Element, name: &str) {
     element
         .remove_attribute(name)
         .expect("can't remove attribute");
+}
+// endregion
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use wasm_bindgen_test::*;
+    wasm_bindgen_test_configure!(run_in_browser);
+
+    fn get_new_document() -> Document {
+        Document::new().unwrap()
+    }
+
+    // region Get elements
+    #[wasm_bindgen_test]
+    fn should_get_window() {
+        get_window();
+    }
+
+    #[wasm_bindgen_test]
+    fn should_get_document() {
+        get_document();
+    }
+
+    #[wasm_bindgen_test]
+    fn should_get_element_by_id() {
+        let id = "id";
+
+        let document = get_new_document();
+        let element = document.create_element("p").unwrap();
+        element.set_id(id);
+
+        document.get_root_node().append_child(&element).unwrap();
+
+        get_element_by_id(&document, id);
+    }
+
+    #[wasm_bindgen_test]
+    #[should_panic(expected = "element does not exist")]
+    fn should_not_get_element_by_id() {
+        let id = "id";
+
+        let document = get_new_document();
+        get_element_by_id(&document, id);
+    }
+
+    #[wasm_bindgen_test]
+    fn should_get_element_by_id_dyn() {
+        let id = "id";
+        let document = get_new_document();
+        let element = document.create_element("p").unwrap();
+        element.set_id(id);
+
+        document.get_root_node().append_child(&element).unwrap();
+        get_element_by_id_dyn::<Element>(&document, id);
+    }
+
+    #[wasm_bindgen_test]
+    #[should_panic(expected = "element does not exist")]
+    fn should_not_get_element_by_id_dyn() {
+        let id = "id";
+
+        let document = get_new_document();
+        get_element_by_id_dyn::<Element>(&document, id);
+    }
+
+    #[wasm_bindgen_test]
+    fn should_get_value_from_input() {
+        let id = "id";
+        let value = "value";
+
+        let document = get_new_document();
+        let element = document.create_element("p").unwrap();
+        element.set_id(id);
+        element.set_attribute("value", value).unwrap();
+
+        document.get_root_node().append_child(&element).unwrap();
+
+        assert_eq!(value, get_value_from_input(&document, id));
+    }
+
+    #[wasm_bindgen_test]
+    #[should_panic(expected = "element does not exist")]
+    fn should_not_get_value_when_element_does_not_exist() {
+        let id = "id";
+
+        let document = get_new_document();
+        get_value_from_input(&document, id);
+    }
+
+    #[wasm_bindgen_test]
+    #[should_panic(expected = "input does not contain text")]
+    fn should_not_get_value_when_element_does_not_not_contain_text() {
+        let id = "id";
+
+        let document = get_new_document();
+        let element = document.create_element("p").unwrap();
+        element.set_id(id);
+
+        document.get_root_node().append_child(&element).unwrap();
+
+        get_value_from_input(&document, id);
+    }
+    // endregion
+
+    // region Create elements
+    #[wasm_bindgen_test]
+    fn should_create_element() {
+        let document = Document::new().unwrap();
+        let name = "p";
+        let parent = document.create_element("p").unwrap();
+        let inner_html = Some("some text");
+
+        let new_element = create_element(&document, name, Some(&parent), inner_html);
+
+        assert_eq!(name, new_element.tag_name());
+        assert_eq!(parent, new_element.parent_element().unwrap());
+        assert_eq!(inner_html.unwrap(), new_element.inner_html());
+    }
+
+    #[wasm_bindgen_test]
+    fn should_create_element_without_parent_or_text() {
+        let document = Document::new().unwrap();
+        let name = "p";
+
+        let new_element = create_element(&document, name, None, None);
+
+        assert_eq!(name, new_element.tag_name());
+        assert_eq!(None, new_element.parent_element());
+        assert_eq!("", new_element.inner_html());
+    }
+
+    #[wasm_bindgen_test]
+    fn should_create_element_with_class() {
+        let document = Document::new().unwrap();
+        let name = "p";
+        let parent = document.create_element("p").unwrap();
+        let inner_html = Some("some text");
+        let class = "class";
+
+        let new_element =
+            create_element_with_class(&document, name, Some(&parent), inner_html, class);
+
+        assert_eq!(name, new_element.tag_name());
+        assert_eq!(parent, new_element.parent_element().unwrap());
+        assert_eq!(inner_html.unwrap(), new_element.inner_html());
+        assert_eq!(class, new_element.class_name());
+    }
+
+    #[wasm_bindgen_test]
+    fn should_create_element_with_classes() {
+        let document = Document::new().unwrap();
+        let name = "p";
+        let parent = document.create_element("p").unwrap();
+        let inner_html = Some("some text");
+        let classes = ["class1", "class2"];
+
+        let new_element =
+            create_element_with_classes(&document, name, Some(&parent), inner_html, &classes);
+
+        assert_eq!(name, new_element.tag_name());
+        assert_eq!(parent, new_element.parent_element().unwrap());
+        assert_eq!(inner_html.unwrap(), new_element.inner_html());
+        assert_eq!(classes.join(" "), new_element.class_name());
+    }
+    // endregion
+
+    // region Manipulate existing elements
+    #[wasm_bindgen_test]
+    fn should_append_chile() {
+        let document = Document::new().unwrap();
+        let child = document.create_element("p").unwrap();
+        let container = document.create_element("p").unwrap();
+
+        append_child(&container, &child);
+    }
+
+    #[wasm_bindgen_test]
+    fn should_clear_element() {
+        let document = Document::new().unwrap();
+        let element = document.create_element("p").unwrap();
+        let value = "value";
+        element.set_inner_html(value);
+
+        assert_eq!(value, element.inner_html());
+
+        clear_element(&element);
+
+        assert_eq!("", element.inner_html());
+    }
+
+    #[wasm_bindgen_test]
+    fn should_set_attribute() {
+        let document = Document::new().unwrap();
+        let element = document.create_element("p").unwrap();
+        let key = "key";
+        let value = "value";
+
+        assert_eq!(None, element.get_attribute(key));
+        set_attribute(&element, key, value);
+        assert_eq!(value, element.get_attribute(key).unwrap());
+    }
+
+    #[wasm_bindgen_test]
+    fn should_remove_attribute() {
+        let document = Document::new().unwrap();
+        let element = document.create_element("p").unwrap();
+        let key = "key";
+        let value = "value";
+
+        element
+            .set_attribute(key, value)
+            .expect("can't set attribute");
+        assert_eq!(value, element.get_attribute(key).unwrap());
+        remove_attribute(&element, key);
+        assert_eq!(None, element.get_attribute(key));
+    }
+    // endregion
 }
