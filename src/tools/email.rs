426997aebc4cf3ddcfd2e2c83a63b38cc36a1b17
Maxence Cornaton <maxence.cornaton+git@proton.me>
2025-03-06 16:51:37 +0100

    Split methods in `email.rs`

--- src/tools/email.rs
+++ src/tools/email.rs
@@ -2,15 +2,19 @@ use crate::tools::email::Error::{
     CantConnectToSmtpServer, CantSendMessage, MissingEmailSenderAddress, MissingEmailSenderName,
     MissingSmtpLogin, MissingSmtpPassword,
 };
-use crate::tools::env_args::retrieve_arg_value;
+use crate::tools::env_args::retrieve_expected_arg_value;
 use crate::tools::log_message_and_return;
 use mail_send::SmtpClientBuilder;
 use mail_send::mail_builder::MessageBuilder;
 
 type Result<T, E = Error> = std::result::Result<T, E>;
 
-const EMAIL_SENDER_NAME_ARG: &'static str = "--email_sender_name";
-const EMAIL_SENDER_ADDRESS_ARG: &'static str = "--email_address";
+const EMAIL_SENDER_NAME_ARG: &'static str = "--email-sender-name";
+const EMAIL_SENDER_ADDRESS_ARG: &'static str = "--email-address";
+const SMTP_LOGIN_ARG: &'static str = "--smtp-login";
+const SMTP_PASSWORD_ARG: &'static str = "--smtp-password";
+const SMTP_SERVER: &'static str = "smtp.gmail.com";
+const SMTP_PORT: u16 = 587;
 
 pub async fn send_email(
     recipients: &[(&str, &str)],
@@ -19,15 +23,15 @@ pub async fn send_email(
     text_body: &str,
 ) -> Result<()> {
     let message = create_message(recipients, subject, html_body, text_body)?;
+    create_smtp_client_and_send_email(message).await
+}
 
-    const GMAIL_SMTP_SERVER: &'static str = "smtp.gmail.com";
-    const GMAIL_SMTP_PORT: u16 = 587;
-    let gmail_smtp_login = retrieve_arg_value(&["--email_sender_name"]).ok_or(MissingSmtpLogin)?;
-    let gmail_smtp_password =
-        retrieve_arg_value(&["--email_sender_name"]).ok_or(MissingSmtpPassword)?;
-    let smtp_client = SmtpClientBuilder::new(GMAIL_SMTP_SERVER, GMAIL_SMTP_PORT)
+async fn create_smtp_client_and_send_email(message: MessageBuilder<'_>) -> Result<()> {
+    let smtp_login = retrieve_smtp_login()?;
+    let smtp_password = retrieve_smtp_password()?;
+    let smtp_client = SmtpClientBuilder::new(SMTP_SERVER, SMTP_PORT)
         .implicit_tls(false)
-        .credentials((gmail_smtp_login.as_str(), gmail_smtp_password.as_str()))
+        .credentials((smtp_login.as_str(), smtp_password.as_str()))
         .connect()
         .await;
 
@@ -50,9 +54,8 @@ fn create_message<'a>(
     html_body: &'a str,
     text_body: &'a str,
 ) -> Result<MessageBuilder<'a>> {
-    let sender_name = retrieve_arg_value(&[EMAIL_SENDER_NAME_ARG]).ok_or(MissingEmailSenderName)?;
-    let sender_address =
-        retrieve_arg_value(&[EMAIL_SENDER_ADDRESS_ARG]).ok_or(MissingEmailSenderAddress)?;
+    let sender_name = retrieve_email_sender_name()?;
+    let sender_address = retrieve_email_sender_address()?;
 
     Ok(MessageBuilder::new()
         .from((sender_name, sender_address))
@@ -61,6 +64,24 @@ fn create_message<'a>(
         .html_body(html_body)
         .text_body(text_body))
 }
+
+// region Retrieve args
+fn retrieve_smtp_login() -> Result<String> {
+    retrieve_expected_arg_value(SMTP_LOGIN_ARG, MissingSmtpLogin)
+}
+
+fn retrieve_smtp_password() -> Result<String> {
+    retrieve_expected_arg_value(SMTP_PASSWORD_ARG, MissingSmtpPassword)
+}
+
+fn retrieve_email_sender_name() -> Result<String> {
+    retrieve_expected_arg_value(EMAIL_SENDER_NAME_ARG, MissingEmailSenderName)
+}
+
+fn retrieve_email_sender_address() -> Result<String> {
+    retrieve_expected_arg_value(EMAIL_SENDER_ADDRESS_ARG, MissingEmailSenderAddress)
+}
+// endregion
 
 #[derive(Debug, PartialEq)]
 pub enum Error {
@@ -110,7 +131,7 @@ mod tests {
 
     #[parameterized(
         args = {
-            vec![format!("{EMAIL_SENDER_NAME_ARG}=sender")],
+            vec![format!("{EMAIL_SENDER_NAME_ARG}=Sender")],
             vec![format!("{EMAIL_SENDER_ADDRESS_ARG}=sender@address.com")],
             vec![],
         },
@@ -132,6 +153,74 @@ mod tests {
 
         let error = result.unwrap_err();
         assert_eq!(expected_error, error);
+    }
+    // endregion
+
+    // region Retrieve args
+    #[parameterized(
+        args = {
+            vec![format!("{SMTP_LOGIN_ARG}=login")],
+            vec![format!("{SMTP_PASSWORD_ARG}=password")],
+            vec![format!("{EMAIL_SENDER_NAME_ARG}=Sender")],
+            vec![format!("{EMAIL_SENDER_ADDRESS_ARG}=sender@address.com")],
+            vec![format!("{SMTP_LOGIN_ARG}=login"), format!("{SMTP_PASSWORD_ARG}=password")],
+        },
+        function = {
+            &retrieve_smtp_login,
+            &retrieve_smtp_password,
+            &retrieve_email_sender_name,
+            &retrieve_email_sender_address,
+            &retrieve_smtp_login
+        },
+        expected_result = {
+            "login".to_owned(),
+            "password".to_owned(),
+            "Sender".to_owned(),
+            "sender@address.com".to_owned(),
+            "login".to_owned(),
+        }
+    )]
+    fn should_retrieve_arg(
+        args: Vec<String>,
+        function: &dyn Fn() -> Result<String>,
+        expected_result: String,
+    ) {
+        let result = with_env_args(args, function).unwrap();
+
+        assert_eq!(expected_result, result);
+    }
+
+    #[parameterized(
+        args = {
+            vec![format!("{SMTP_LOGIN_ARG}=login")],
+            vec![format!("{SMTP_PASSWORD_ARG}=password")],
+            vec![format!("{EMAIL_SENDER_NAME_ARG}=Sender")],
+            vec![format!("{EMAIL_SENDER_ADDRESS_ARG}=sender@address.com")],
+            vec![format!("{SMTP_LOGIN_ARG}=login"), format!("{SMTP_PASSWORD_ARG}=password")],
+        },
+        function = {
+            &retrieve_smtp_login,
+            &retrieve_smtp_password,
+            &retrieve_email_sender_name,
+            &retrieve_email_sender_address,
+            &retrieve_smtp_login
+        },
+        expected_result = {
+            "login".to_owned(),
+            "password".to_owned(),
+            "Sender".to_owned(),
+            "sender@address.com".to_owned(),
+            "login".to_owned(),
+        }
+    )]
+    fn should_fail_to_retrieve_arg(
+        args: Vec<String>,
+        function: &dyn Fn() -> Result<String>,
+        expected_result: String,
+    ) {
+        let result = with_env_args(args, function).unwrap();
+
+        assert_eq!(expected_result, result);
     }
     // endregion
 }
