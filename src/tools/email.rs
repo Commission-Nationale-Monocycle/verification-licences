b8475283c074c850eb970d8d6534d0fa87738c37
Maxence Cornaton <22578053+maxence-cornaton@users.noreply.github.com>
2025-03-07 13:31:57 +0100

    Sending emails feature (#41)
    * Introduce mail sending feature
    
    * Enhance retrieve arg feature
    
    * Split methods in `email.rs`
    
    * Add REPLY_TO and BCC fields to emails
    
    * Introduce args for SMTP server and port
    
    * Add all args to README.md
    
    * Fix cURL example request
    
    * Add tests for new args retrieval
    
    * Add test for email sending feature
    
    * Allow dead code on sending email feature
    
    * Fix clippy warnings
    
    * Fix and comment secrets in Github action
    
    * Re-enable send mail test with Tarpaulin
    
    * Try fixing cargo tarpaulin command in Github action
    
    * Fix tests
    
    * Fix cargo tarpaulin command in Github workflow
    
    * Remove Docker build on each PR
    
    ---------
    
    Co-authored-by: Maxence Cornaton <maxence.cornaton+git@proton.me>

---
+++ src/tools/email.rs
@@ -0,0 +1,313 @@
+use crate::tools::email::Error::{
+    CantConnectToSmtpServer, CantSendMessage, MissingEmailSenderAddress, MissingEmailSenderName,
+    MissingSmtpLogin, MissingSmtpPassword,
+};
+use crate::tools::env_args::{retrieve_arg_value, retrieve_expected_arg_value};
+use crate::tools::log_message_and_return;
+use mail_send::SmtpClientBuilder;
+use mail_send::mail_builder::MessageBuilder;
+
+type Result<T, E = Error> = std::result::Result<T, E>;
+
+const EMAIL_SENDER_NAME_ARG: &str = "--email-sender-name";
+const EMAIL_SENDER_ADDRESS_ARG: &str = "--email-sender_address";
+const REPLY_TO_ARG: &str = "--reply-to";
+const SMTP_SERVER_ARG: &str = "--smtp-server";
+const SMTP_PORT_ARG: &str = "--smtp-port";
+const SMTP_LOGIN_ARG: &str = "--smtp-login";
+const SMTP_PASSWORD_ARG: &str = "--smtp-password";
+const DEFAULT_SMTP_SERVER: &str = "smtp.gmail.com";
+const DEFAULT_SMTP_PORT: u16 = 25;
+
+#[allow(dead_code)]
+pub async fn send_email(
+    recipients: &[&str],
+    subject: &str,
+    html_body: &str,
+    text_body: &str,
+) -> Result<()> {
+    let message = create_message(recipients, subject, html_body, text_body)?;
+    create_smtp_client_and_send_email(message).await
+}
+
+async fn create_smtp_client_and_send_email(message: MessageBuilder<'_>) -> Result<()> {
+    let smtp_server = retrieve_smtp_server();
+    let smtp_port = retrieve_smtp_port();
+    let smtp_login = retrieve_smtp_login()?;
+    let smtp_password = retrieve_smtp_password()?;
+    let smtp_client = SmtpClientBuilder::new(smtp_server, smtp_port)
+        .implicit_tls(false)
+        .credentials((smtp_login, smtp_password))
+        .connect()
+        .await;
+
+    smtp_client
+        .map_err(log_message_and_return(
+            "Couldn't connect to SMTP server",
+            CantConnectToSmtpServer,
+        ))?
+        .send(message)
+        .await
+        .map_err(log_message_and_return(
+            "Couldn't send message",
+            CantSendMessage,
+        ))
+}
+
+fn create_message<'a>(
+    recipients: &'a [&str],
+    subject: &'a str,
+    html_body: &'a str,
+    text_body: &'a str,
+) -> Result<MessageBuilder<'a>> {
+    let sender_name = retrieve_email_sender_name()?;
+    let sender_address = retrieve_email_sender_address()?;
+    let reply_to_address = retrieve_reply_to().unwrap_or_else(|| sender_address.clone());
+
+    Ok(MessageBuilder::new()
+        .from((sender_name, sender_address))
+        .reply_to(reply_to_address.clone())
+        .to(reply_to_address)
+        .bcc(Vec::from(recipients))
+        .subject(subject)
+        .html_body(html_body)
+        .text_body(text_body))
+}
+
+// region Retrieve args
+fn retrieve_smtp_server() -> String {
+    retrieve_arg_value(SMTP_SERVER_ARG).unwrap_or(DEFAULT_SMTP_SERVER.to_owned())
+}
+fn retrieve_smtp_port() -> u16 {
+    retrieve_arg_value(SMTP_PORT_ARG)
+        .and_then(|port| port.parse::<u16>().ok())
+        .unwrap_or(DEFAULT_SMTP_PORT)
+}
+fn retrieve_smtp_login() -> Result<String> {
+    retrieve_expected_arg_value(SMTP_LOGIN_ARG, MissingSmtpLogin)
+}
+
+fn retrieve_smtp_password() -> Result<String> {
+    retrieve_expected_arg_value(SMTP_PASSWORD_ARG, MissingSmtpPassword)
+}
+
+fn retrieve_email_sender_name() -> Result<String> {
+    retrieve_expected_arg_value(EMAIL_SENDER_NAME_ARG, MissingEmailSenderName)
+}
+
+fn retrieve_email_sender_address() -> Result<String> {
+    retrieve_expected_arg_value(EMAIL_SENDER_ADDRESS_ARG, MissingEmailSenderAddress)
+}
+
+fn retrieve_reply_to() -> Option<String> {
+    retrieve_arg_value(REPLY_TO_ARG)
+}
+// endregion
+
+#[derive(Debug, PartialEq)]
+pub enum Error {
+    MissingEmailSenderName,
+    MissingEmailSenderAddress,
+    MissingSmtpLogin,
+    MissingSmtpPassword,
+    CantConnectToSmtpServer,
+    CantSendMessage,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::tools::env_args::with_env_args;
+    use mail_send::mail_builder::mime::BodyPart;
+    use parameterized::{ide, parameterized};
+    use rocket::futures::executor::block_on;
+
+    ide!();
+
+    const TEST_SMTP_SERVER: &str = "sandbox.smtp.mailtrap.io";
+    const TEST_SMTP_PORT: u16 = 25;
+    const TEST_EMAIL_SENDER_NAME: &str = "Sender";
+    const TEST_EMAIL_SENDER_ADDRESS: &str = "sender@address.com";
+    const TEST_REPLY_TO: &str = "sender+reply-to@address.com";
+    const TEST_RECIPIENTS: &[&str] = &["recipient@address.com"];
+    const TEST_SUBJECT: &str = "This is a subject";
+    const TEST_HTML_BODY: &str = "<h1>This is a very important email</h1>";
+    const TEST_TEXT_BODY: &str = "This is a slightly less important email";
+
+    fn get_args() -> Vec<String> {
+        vec![
+            format!("{SMTP_SERVER_ARG}={TEST_SMTP_SERVER}"),
+            format!("{SMTP_PORT_ARG}={TEST_SMTP_PORT}"),
+            format!("{EMAIL_SENDER_NAME_ARG}={TEST_EMAIL_SENDER_NAME}"),
+            format!("{EMAIL_SENDER_ADDRESS_ARG}={TEST_EMAIL_SENDER_ADDRESS}"),
+            format!("{REPLY_TO_ARG}={TEST_REPLY_TO}"),
+        ]
+    }
+
+    // region send_email
+    #[async_test]
+    async fn should_send_email() {
+        let args = get_args();
+        with_env_args(args, || block_on(send_email(
+            TEST_RECIPIENTS,
+            TEST_SUBJECT,
+            TEST_HTML_BODY,
+            TEST_TEXT_BODY,
+        ))).unwrap();
+    }
+    // endregion
+
+    // region create_message
+    #[test]
+    fn should_create_message() {
+        let sender_name = "Sender";
+        let sender_address = "sender@address.com";
+        let sender_name_arg = format!("{EMAIL_SENDER_NAME_ARG}={sender_name}");
+        let sender_address_arg = format!("{EMAIL_SENDER_ADDRESS_ARG}={sender_address}");
+        let args = vec![sender_name_arg, sender_address_arg];
+
+        let function = || {
+            create_message(
+                TEST_RECIPIENTS,
+                TEST_SUBJECT,
+                TEST_HTML_BODY,
+                TEST_TEXT_BODY,
+            )
+        };
+        let result = with_env_args(args, function);
+
+        assert!(result.is_ok());
+        let result = result.unwrap();
+        match result.clone().text_body.unwrap().contents {
+            BodyPart::Text(text) => assert_eq!(TEST_TEXT_BODY, text),
+            BodyPart::Binary(_) => panic!("Unexpected binary part"),
+            BodyPart::Multipart(_) => panic!("Unexpected multipart part"),
+        };
+    }
+
+    #[parameterized(
+        args = {
+            vec![format!("{EMAIL_SENDER_NAME_ARG}={TEST_EMAIL_SENDER_NAME}")],
+            vec![format!("{EMAIL_SENDER_ADDRESS_ARG}={TEST_EMAIL_SENDER_ADDRESS}")],
+            vec![],
+        },
+        expected_error = {
+            MissingEmailSenderAddress,
+            MissingEmailSenderName,
+            MissingEmailSenderName,
+        }
+    )]
+    fn should_fail_to_create_message(args: Vec<String>, expected_error: Error) {
+        let function = || {
+            create_message(
+                TEST_RECIPIENTS,
+                TEST_SUBJECT,
+                TEST_HTML_BODY,
+                TEST_TEXT_BODY,
+            )
+        };
+        let result = with_env_args(args, function);
+
+        let error = result.unwrap_err();
+        assert_eq!(expected_error, error);
+    }
+    // endregion
+
+    // region Retrieve args
+    #[parameterized(
+        args = {
+            vec![format!("{SMTP_SERVER_ARG}={TEST_SMTP_SERVER}")],
+            vec![format!("{SMTP_PORT_ARG}={TEST_SMTP_PORT}")],
+        },
+        function = {
+            &retrieve_smtp_server,
+            & || retrieve_smtp_port().to_string(),
+        },
+        expected_result = {
+            TEST_SMTP_SERVER.to_owned(),
+            TEST_SMTP_PORT.to_string(),
+        }
+    )]
+    fn should_retrieve_optional_arg(
+        args: Vec<String>,
+        function: &dyn Fn() -> String,
+        expected_result: String,
+    ) {
+        let result = with_env_args(args, function);
+
+        assert_eq!(expected_result, result);
+    }
+
+    #[parameterized(
+        args = {
+            vec![],
+            vec![],
+        },
+        function = {
+            &retrieve_smtp_server,
+            & || retrieve_smtp_port().to_string(),
+        },
+        expected_result = {
+            DEFAULT_SMTP_SERVER.to_owned(),
+            DEFAULT_SMTP_PORT.to_string(),
+        }
+    )]
+    fn should_retrieve_default_value_for_optional_arg(
+        args: Vec<String>,
+        function: &dyn Fn() -> String,
+        expected_result: String,
+    ) {
+        let result = with_env_args(args, function);
+
+        assert_eq!(expected_result, result);
+    }
+
+    #[parameterized(
+        args = {
+            vec![format!("{EMAIL_SENDER_NAME_ARG}={TEST_EMAIL_SENDER_NAME}")],
+            vec![format!("{EMAIL_SENDER_ADDRESS_ARG}={TEST_EMAIL_SENDER_ADDRESS}")],
+        },
+        function = {
+            &retrieve_email_sender_name,
+            &retrieve_email_sender_address,
+        },
+        expected_result = {
+            TEST_EMAIL_SENDER_NAME.to_owned(),
+            TEST_EMAIL_SENDER_ADDRESS.to_owned(),
+        }
+    )]
+    fn should_retrieve_expected_arg(
+        args: Vec<String>,
+        function: &dyn Fn() -> Result<String>,
+        expected_result: String,
+    ) {
+        let result = with_env_args(args, function).unwrap();
+
+        assert_eq!(expected_result, result);
+    }
+
+    #[parameterized(
+        args = {
+            vec![format!("{EMAIL_SENDER_NAME_ARG}={TEST_EMAIL_SENDER_NAME}")],
+            vec![format!("{EMAIL_SENDER_ADDRESS_ARG}={TEST_EMAIL_SENDER_ADDRESS}")],
+        },
+        function = {
+            &retrieve_email_sender_name,
+            &retrieve_email_sender_address,
+        },
+        expected_result = {
+            TEST_EMAIL_SENDER_NAME.to_owned(),
+            TEST_EMAIL_SENDER_ADDRESS.to_owned(),
+        }
+    )]
+    fn should_fail_to_retrieve_arg(
+        args: Vec<String>,
+        function: &dyn Fn() -> Result<String>,
+        expected_result: String,
+    ) {
+        let result = with_env_args(args, function).unwrap();
+
+        assert_eq!(expected_result, result);
+    }
+    // endregion
+}
--- src/tools/env_args.rs
+++ src/tools/env_args.rs
@@ -2,6 +2,35 @@
 use std::cell::RefCell;
 #[cfg(not(test))]
 use std::env;
+use std::ops::Deref;
+
+// region ArgName
+/// Simple wrapper around a collection of strings.
+/// Can be constructed automatically from &str & Vec<&str>.
+/// Useful to handle args which can have multiple names and those which can have no more than one name.
+pub struct ArgName<'a> {
+    names: Vec<&'a str>,
+}
+impl<'a> From<&'a str> for ArgName<'a> {
+    fn from(val: &'a str) -> Self {
+        ArgName { names: vec![val] }
+    }
+}
+
+impl<'a> From<Vec<&'a str>> for ArgName<'a> {
+    fn from(val: Vec<&'a str>) -> Self {
+        ArgName { names: val }
+    }
+}
+
+impl<'a> Deref for ArgName<'a> {
+    type Target = Vec<&'a str>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.names
+    }
+}
+// endregion
 
 /// Retrieve value associated to an arg passed to the app.
 ///
@@ -9,10 +38,14 @@ use std::env;
 /// a function using `retrieve_arg_value` could be tricky to test.
 /// To do so, wrap your test with `with_env_args(args, fn)`.
 /// This function is only available in a test context.
-pub fn retrieve_arg_value(arg_names: &[&str]) -> Option<String> {
+pub fn retrieve_arg_value<'a, A>(arg_names: A) -> Option<String>
+where
+    A: Into<ArgName<'a>>,
+{
     let args: Vec<String> = get_env_args();
+    let arg_names = arg_names.into();
     for arg in args {
-        for arg_name in arg_names {
+        for arg_name in arg_names.iter() {
             let arg_prefix = format!("{arg_name}=");
             if arg.starts_with(&arg_prefix) {
                 return arg.split_once("=").map(|(_, l)| l.to_owned());
@@ -21,6 +54,11 @@ pub fn retrieve_arg_value(arg_names: &[&str]) -> Option<String> {
     }
 
     None
+}
+
+/// Retrieve an arg value
+pub fn retrieve_expected_arg_value<E>(arg_name: &str, error_if_missing: E) -> Result<String, E> {
+    retrieve_arg_value(arg_name).ok_or(error_if_missing)
 }
 
 #[cfg(not(test))]
@@ -42,13 +80,15 @@ fn get_env_args() -> Vec<String> {
 }
 
 #[cfg(test)]
-/// When running tests, env args are sourced from within the app.
+/// When running tests, env args are extended from within the app.
 /// You can set them up from there by wrapping your test with this function.
-pub fn with_env_args<F, T>(args: Vec<String>, function: F) -> T
+pub fn with_env_args<F, T>(mut args: Vec<String>, function: F) -> T
 where
     F: FnOnce() -> T,
 {
     ENV_ARGS.with(|refcell| {
+        let global_env_args = std::env::args().collect::<Vec<String>>();
+        args.extend_from_slice(&global_env_args);
         let old_value = refcell.replace(args);
         let result = function();
         refcell.replace(old_value);
@@ -60,21 +100,43 @@ where
 pub mod tests {
     use parameterized::{ide, parameterized};
 
-    use crate::tools::env_args::{retrieve_arg_value, with_env_args};
+    use crate::tools::env_args::{retrieve_arg_value, retrieve_expected_arg_value, with_env_args};
 
     ide!();
 
     #[parameterized(
-        args = {vec ! ["-l=test_login".to_owned()], vec ! ["--login=test_login".to_owned()], vec ! ["-p=test_password".to_owned()], vec ! ["--password=test_password".to_owned()], vec ! ["--another-arg=wrong".to_owned()]},
-        arg_names = {& ["-l", "--login"], & ["-l", "--login"], & ["-p", "--password"], & ["-p", "--password"], & ["-p", "--password"]},
+        args = {vec!["-l=test_login".to_owned()], vec!["--login=test_login".to_owned()], vec!["-p=test_password".to_owned()], vec!["--password=test_password".to_owned()], vec!["--another-arg=wrong".to_owned()]},
+        arg_names = {vec!["-l", "--login"], vec!["-l", "--login"], vec!["-p", "--password"], vec!["-p", "--password"], vec!["-p", "--password"]},
         expected_result = {Some("test_login".to_owned()), Some("test_login".to_owned()), Some("test_password".to_owned()), Some("test_password".to_owned()), None}
     )]
     fn should_retrieve_arg_value(
         args: Vec<String>,
-        arg_names: &[&str],
+        arg_names: Vec<&str>,
         expected_result: Option<String>,
     ) {
         let result = with_env_args(args, || retrieve_arg_value(arg_names));
         assert_eq!(expected_result, result);
+    }
+
+    #[test]
+    fn should_retrieve_expected_arg_value() {
+        let arg_name = "--arg-name";
+        let arg_value = "arg-value";
+        let error = "error!";
+        let args = vec![format!("{arg_name}={arg_value}")];
+
+        let result = with_env_args(args, || retrieve_expected_arg_value(arg_name, error)).unwrap();
+
+        assert_eq!(arg_value, result);
+    }
+
+    #[test]
+    fn should_fail_to_retrieve_expected_arg_value() {
+        let arg_name = "arg-name";
+        let error = "error!";
+
+        let result = retrieve_expected_arg_value(arg_name, error).unwrap_err();
+
+        assert_eq!(error, result);
     }
 }
