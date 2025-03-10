631f6d8ce34388ffa936b49a987ea922eaef8606
Maxence Cornaton <maxence.cornaton+git@proton.me>
2025-03-10 14:20:09 +0100

    Setup Flowbite

--- build-css.bat
+++ build-css.bat
@@ -1,1 +1,1 @@
-npx @tailwindcss/cli -i public/styles/styles.css -o public/static/styles.css
+npx @tailwindcss/cli -i public/styles/styles.css -o public/static/styles.css --minify
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,14 @@ module.exports = {
     future: {},
     purge: [
         "./public/**/*.html.tera",
-        "./wasm/**/*.rs"]
-    ,
+        "./wasm/**/*.rs",
+    ],
     theme: {
         extend: {},
     },
     variants: {},
     plugins: [
         require('tailwindcss'),
-        require("autoprefixer"),
+        require("autoprefixer")
     ],
 }
