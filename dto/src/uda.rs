031a82b505461a28a1f49d2ba32462b9ddc665e4
Maxence Cornaton <22578053+maxence-cornaton@users.noreply.github.com>
2025-03-18 16:47:14 +0100

    Store UDA instances list as a state (#66)
    Co-authored-by: Maxence Cornaton <maxence.cornaton+git@proton.me>

--- dto/src/uda.rs
+++ dto/src/uda.rs
@@ -1,3 +1,4 @@
+use chrono::{NaiveDate, Utc};
 use derive_getters::Getters;
 use serde::{Deserialize, Serialize};
 
@@ -11,5 +12,18 @@ pub struct Instance {
 impl Instance {
     pub fn new(slug: String, name: String, url: String) -> Self {
         Self { slug, name, url }
+    }
+}
+
+#[derive(Debug, Getters, Default, PartialEq, Serialize, Deserialize)]
+pub struct InstancesList {
+    instances: Vec<Instance>,
+    update_date: Option<NaiveDate>,
+}
+
+impl InstancesList {
+    pub fn set_instances(&mut self, instances: Vec<Instance>) {
+        self.instances = instances;
+        self.update_date = Some(Utc::now().date_naive());
     }
 }
