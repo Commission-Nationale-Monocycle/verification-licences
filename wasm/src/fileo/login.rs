f059d65dc83053588d4577d7a558818722d1807e
Maxence Cornaton <maxence.cornaton+git@proton.me>
2025-03-19 10:43:55 +0100

    Replace Reqwest with simple AJAX requests

--- wasm/src/fileo/login.rs
+++ wasm/src/fileo/login.rs
@@ -1,11 +1,8 @@
 use crate::alert::{AlertLevel, create_alert, unwrap_or_alert, unwrap_without_alert};
-use crate::build_client;
 use crate::error::Error;
 use crate::user_interface::set_loading;
-use crate::utils::{
-    get_document, get_element_by_id_dyn, get_location, get_value_from_element, get_window,
-};
-use reqwest::StatusCode;
+use crate::utils::{get_document, get_element_by_id_dyn, get_location, get_value_from_element};
+use crate::web::fetch;
 use serde_json::json;
 use wasm_bindgen::JsCast;
 use wasm_bindgen::closure::Closure;
@@ -50,76 +47,68 @@ pub async fn login() {
 
     let login = get_value_from_element(&login_field);
     let password = get_value_from_element(&password_field);
-
-    let client = build_client();
-    let origin = unwrap_without_alert(get_window())
-        .location()
-        .origin()
-        .unwrap();
-    let url = format!("{origin}/api/fileo/login");
+    let url = "/api/fileo/login";
     let body = json!({
         "login": login, "password": password
     })
     .to_string();
-    let response = client
-        .post(&url)
-        .header("Content-Type", "application/json")
-        .body(body)
-        .send()
-        .await
-        .unwrap_or_else(|error| {
+
+    match fetch(url, "post", Some("application/json"), Some(&body)).await {
+        Ok(response) => {
+            let status = response.status();
+            if (200..400).contains(&status) {
+                unwrap_or_alert(set_loading(false));
+                let location = unwrap_or_alert(get_location());
+                let query_params = unwrap_or_alert(location.search().map_err(|error| {
+                    Error::from_parent(
+                        "Erreur, veuillez réessayer.".to_owned(),
+                        Error::new(error.as_string().unwrap()),
+                    )
+                }));
+                let query_params = unwrap_or_alert(
+                    UrlSearchParams::new_with_str(&query_params).map_err(|error| {
+                        Error::from_parent(
+                            "Erreur, veuillez réessayer.".to_owned(),
+                            Error::new(error.as_string().unwrap()),
+                        )
+                    }),
+                );
+                let url_to_redirect = if let Some(redirect) = query_params.get("page") {
+                    redirect
+                } else {
+                    "/check-memberships".to_owned()
+                };
+                let result = location.set_href(&url_to_redirect);
+                if let Err(error) = result {
+                    create_alert(
+                        "Erreur lors de la redirection. Veuillez actualiser la page.",
+                        AlertLevel::Error,
+                    );
+                    log::error!("Can't redirect user: {error:?}");
+                }
+            } else if status == 401 {
+                unwrap_or_alert(set_loading(false));
+                create_alert(
+                    "Vos identifiants sont incorrects. Veuillez réessayer.",
+                    AlertLevel::Error,
+                );
+            } else {
+                unwrap_or_alert(set_loading(false));
+                create_alert(
+                    "Impossible de se connecter. Veuillez réessayer.",
+                    AlertLevel::Error,
+                );
+                log::error!("Server error: {}", status);
+            }
+        }
+        Err(error) => {
             unwrap_or_alert(set_loading(false));
             create_alert(
-                "Impossible d'envoyer la requête. Veuillez réessayer.",
+                "Le serveur a rencontré une erreur lors du traitement. Veuillez réessayer.",
                 AlertLevel::Error,
             );
-            panic!("can't send request: {error:?}")
-        });
-
-    let status = response.status();
-    if status.is_success() {
-        unwrap_or_alert(set_loading(false));
-        let location = unwrap_or_alert(get_location());
-        let query_params = unwrap_or_alert(location.search().map_err(|error| {
-            Error::from_parent(
-                "Erreur, veuillez réessayer.".to_owned(),
-                Error::new(error.as_string().unwrap()),
-            )
-        }));
-        let query_params = unwrap_or_alert(UrlSearchParams::new_with_str(&query_params).map_err(
-            |error| {
-                Error::from_parent(
-                    "Erreur, veuillez réessayer.".to_owned(),
-                    Error::new(error.as_string().unwrap()),
-                )
-            },
-        ));
-        let url_to_redirect = if let Some(redirect) = query_params.get("page") {
-            redirect
-        } else {
-            "/check-memberships".to_owned()
-        };
-        let result = location.set_href(&url_to_redirect);
-        if let Err(error) = result {
-            create_alert(
-                "Erreur lors de la redirection. Veuillez actualiser la page.",
-                AlertLevel::Error,
-            );
-            log::error!("Can't redirect user: {error:?}");
+            log::error!("Server error: {:?}", error);
         }
-    } else if status == StatusCode::UNAUTHORIZED {
-        unwrap_or_alert(set_loading(false));
-        create_alert(
-            "Vos identifiants sont incorrects. Veuillez réessayer.",
-            AlertLevel::Error,
-        );
-    } else {
-        unwrap_or_alert(set_loading(false));
-        create_alert(
-            "Impossible de se connecter. Veuillez réessayer.",
-            AlertLevel::Error,
-        );
-        log::error!("Server error: {}", response.status().as_str());
     }
 }
 
--- wasm/src/uda.rs
+++ wasm/src/uda.rs
@@ -1,43 +1,42 @@
 use crate::alert::{AlertLevel, create_alert, unwrap_or_alert, unwrap_without_alert};
-use crate::build_client;
 use crate::error::Error;
 use crate::user_interface::set_loading;
 use crate::utils::get_window;
+use crate::web::fetch;
 use wasm_bindgen::prelude::wasm_bindgen;
 
 #[wasm_bindgen]
 pub async fn update_uda_instances_list() {
     unwrap_or_alert(set_loading(true));
 
-    let client = build_client();
-
-    let window = unwrap_without_alert(get_window());
-    let location = window.location();
-    let origin = location.origin().unwrap();
-    let url = format!("{origin}/api/uda/instances");
-    let response = client.get(&url).send().await.unwrap_or_else(|error| {
-        unwrap_or_alert(set_loading(false));
-        create_alert(
-            "Impossible d'envoyer la requête. Veuillez réessayer.",
-            AlertLevel::Error,
-        );
-        panic!("can't send request: {error:?}")
-    });
-
-    let status = response.status();
-    if status.is_success() || status.is_redirection() {
-        unwrap_or_alert(
-            location
-                .reload()
-                .map_err(|error| Error::new(format!("Can't reload page: {error:?}"))),
-        );
-        unwrap_or_alert(set_loading(false));
-    } else {
-        unwrap_or_alert(set_loading(false));
-        create_alert(
-            "Le serveur a rencontré une erreur lors du traitement. Veuillez réessayer.",
-            AlertLevel::Error,
-        );
-        log::error!("Server error: {}", response.status().as_str())
+    let url = "/api/uda/instances";
+    match fetch(url, "get", None, None).await {
+        Ok(response) => {
+            let status = response.status();
+            if (200..400).contains(&status) {
+                let location = unwrap_without_alert(get_window()).location();
+                unwrap_or_alert(
+                    location
+                        .reload()
+                        .map_err(|error| Error::new(format!("Can't reload page: {error:?}"))),
+                );
+                unwrap_or_alert(set_loading(false));
+            } else {
+                unwrap_or_alert(set_loading(false));
+                create_alert(
+                    "Le serveur a rencontré une erreur lors du traitement. Veuillez réessayer.",
+                    AlertLevel::Error,
+                );
+                log::error!("Server error: {}", status);
+            }
+        }
+        Err(error) => {
+            unwrap_or_alert(set_loading(false));
+            create_alert(
+                "Le serveur a rencontré une erreur lors du traitement. Veuillez réessayer.",
+                AlertLevel::Error,
+            );
+            log::error!("Server error: {:?}", error);
+        }
     }
 }
