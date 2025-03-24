b2a01b4779137785772767141cef4516cdc78205
Maxence Cornaton <maxence.cornaton+git@proton.me>
2025-03-24 11:23:06 +0100

    Fix login form enter key

--- wasm/src/component/login_form.rs
+++ wasm/src/component/login_form.rs
@@ -1,17 +1,18 @@
-use crate::fileo::login::login;
+use crate::Result;
+use crate::component::alert::unwrap_or_alert;
+use crate::utils::{get_body, query_selector_single_element};
 use wasm_bindgen::JsCast;
 use wasm_bindgen::closure::Closure;
-use wasm_bindgen_futures::spawn_local;
-use web_sys::{Document, KeyboardEvent};
+use web_sys::{Document, Event, KeyboardEvent};
 
 /// Add a simple event handler to allow submitting the login form using any of the Enter keys.
 pub fn init_login_form(document: &Document, login_form_class: &str) {
     let forms = document.get_elements_by_class_name(login_form_class);
-    let closure = Closure::wrap(Box::new(|e: KeyboardEvent| {
-        spawn_local(async move {
-            on_key_down_in_login_form(&e).await;
-        });
-    }) as Box<dyn Fn(_)>);
+    let closure =
+        Closure::wrap(
+            Box::new(|e: KeyboardEvent| unwrap_or_alert(on_key_down_in_login_form(&e)))
+                as Box<dyn Fn(_)>,
+        );
     for i in 0..forms.length() {
         let form = forms.item(i).unwrap();
         form.add_event_listener_with_event_listener("keydown", closure.as_ref().unchecked_ref())
@@ -21,9 +22,12 @@ pub fn init_login_form(document: &Document, login_form_class: &str) {
 }
 
 /// Simple event handler to allow submitting the login form using any of the Enter keys.
-async fn on_key_down_in_login_form(event: &KeyboardEvent) {
+fn on_key_down_in_login_form(event: &KeyboardEvent) -> Result<()> {
     let code = event.code();
     if code == "Enter" || code == "NumpadEnter" {
-        login().await;
+        let button = query_selector_single_element(&get_body()?.into(), ".login-button")?;
+        button.dispatch_event(&Event::new("click")?)?;
     }
+
+    Ok(())
 }
