d6ded86fba3bc726f224d8da70c143dfbcbffde7
Maxence Cornaton <maxence.cornaton+git@proton.me>
2025-04-01 11:50:17 +0200

    When same email multiple times, notify a single time

--- wasm/src/check_memberships.rs
+++ wasm/src/check_memberships.rs
@@ -7,6 +7,7 @@ use crate::user_interface::{get_email_body, get_email_subject, set_loading, with
 use crate::utils::{get_document, get_element_by_id_dyn, query_selector_single_element};
 use crate::web::fetch;
 use dto::email::Email;
+use std::collections::BTreeSet;
 use wasm_bindgen::JsCast;
 use wasm_bindgen::prelude::wasm_bindgen;
 use web_sys::{Document, HtmlButtonElement, HtmlInputElement, HtmlTextAreaElement};
@@ -26,7 +27,10 @@ pub fn toggle_go_to_email_step_button(document: &Document) {
 #[wasm_bindgen]
 pub fn go_to_notification_step(document: &Document) {
     let addresses_to_notify = unwrap_or_alert(get_email_addresses_to_notify(document));
-    let text = addresses_to_notify.join("\n");
+    let text = addresses_to_notify
+        .into_iter()
+        .collect::<Vec<_>>()
+        .join("\n");
     let element = unwrap_or_alert(get_element_by_id_dyn::<HtmlTextAreaElement>(
         document,
         "email-recipients",
@@ -42,7 +46,9 @@ pub fn go_to_notification_step(document: &Document) {
 pub async fn handle_email_sending() {
     with_loading(async || {
         let document = &get_document()?;
-        let email_addresses_to_notify = get_email_addresses_to_notify(document)?;
+        let email_addresses_to_notify = get_email_addresses_to_notify(document)?
+            .into_iter()
+            .collect::<Vec<_>>();
         let email_subject = get_email_subject(document)?;
         let email_body = get_email_body(document)?;
 
@@ -77,10 +83,10 @@ pub async fn handle_email_sending() {
     .await;
 }
 
-fn get_email_addresses_to_notify(document: &Document) -> Result<Vec<String>> {
+fn get_email_addresses_to_notify(document: &Document) -> Result<BTreeSet<String>> {
     // let checked_members_container = user_interface::get_checked_members_container(document);
     let memberships = document.get_elements_by_class_name("membership");
-    let mut email_addresses_to_notify = vec![];
+    let mut email_addresses_to_notify = BTreeSet::new();
     for index in 0..memberships.length() {
         let membership = memberships.get_with_index(index).unwrap();
         if membership.class_name().contains("membership-unknown") {
@@ -95,7 +101,9 @@ fn get_email_addresses_to_notify(document: &Document) -> Result<Vec<String>> {
                 query_selector_single_element(&membership, ".email-address-container a")?;
             match address_container.text_content() {
                 None => set_loading(false)?,
-                Some(email_address) => email_addresses_to_notify.push(email_address),
+                Some(email_address) => {
+                    email_addresses_to_notify.insert(email_address);
+                }
             };
         }
     }
