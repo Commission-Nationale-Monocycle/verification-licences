59d29461dbe74636f7dfb3d53757ef41b00deaea
Maxence Cornaton <maxence.cornaton+git@proton.me>
2025-04-02 17:29:15 +0200

    Introduce Membership DAO

--- src/database/error.rs
+++ src/database/error.rs
@@ -1,4 +1,5 @@
-use crate::database::error::DatabaseError::UnderlyingDatabase;
+use crate::database::error::DatabaseError::{ConversionError, UnderlyingDatabase};
+use chrono::ParseError;
 use std::error::Error;
 use thiserror::Error;
 
@@ -10,10 +11,20 @@ pub enum DatabaseError {
     ConnectionFailed,
     #[error("An error occurred within the database.")]
     UnderlyingDatabase(String),
+    #[error(transparent)]
+    DieselError(#[from] diesel::result::Error),
+    #[error("Can't convert from database value")]
+    ConversionError(String),
 }
 
 impl From<Box<dyn Error + Send + Sync + 'static>> for DatabaseError {
     fn from(value: Box<dyn Error + Send + Sync + 'static>) -> Self {
         UnderlyingDatabase(value.to_string())
+    }
+}
+
+impl From<ParseError> for DatabaseError {
+    fn from(value: ParseError) -> Self {
+        ConversionError(value.to_string())
     }
 }
--- src/database/mod.rs
+++ src/database/mod.rs
@@ -5,8 +5,10 @@ use crate::tools::env_args::retrieve_expected_arg_value;
 use crate::tools::log_error_and_return;
 use diesel::{Connection, SqliteConnection};
 
+mod dao;
 pub(crate) mod error;
 mod migrations;
+mod model;
 mod schema;
 
 pub fn init_db() -> Result<()> {
@@ -15,4 +17,26 @@ pub fn init_db() -> Result<()> {
         .map_err(log_error_and_return(ConnectionFailed))?;
 
     Ok(run_migrations(&mut connection)?)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::database::error::DatabaseError::ConnectionFailed;
+    use crate::database::migrations::MIGRATIONS;
+    use crate::tools::log_error_and_return;
+    use crate::tools::test::tests::temp_dir;
+    use diesel::{Connection, SqliteConnection};
+    use diesel_migrations::MigrationHarness;
+
+    pub fn establish_connection() -> SqliteConnection {
+        let temp_dir = temp_dir();
+        let database_url = temp_dir.join("database.db").to_str().unwrap().to_string();
+        let mut connection = SqliteConnection::establish(&database_url)
+            .map_err(log_error_and_return(ConnectionFailed))
+            .unwrap();
+
+        connection.run_pending_migrations(MIGRATIONS).unwrap();
+
+        connection
+    }
 }
