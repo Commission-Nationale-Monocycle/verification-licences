50ded829d52c46ec370422f58edec3d439b581a0
Maxence Cornaton <22578053+maxence-cornaton@users.noreply.github.com>
2025-05-16 10:37:13 +0200

    Extract UDA DTOs to new repository

--- dto/src/uda_instance.rs
+++ dto/src/uda_instance.rs
@@ -1,31 +1,3 @@
-use chrono::NaiveDate;
-use derive_getters::Getters;
-use serde::{Deserialize, Serialize};
+pub type Instance = uda_dto::uda_instance::Instance;
 
-#[derive(Debug, Getters, PartialEq, Serialize, Deserialize, Clone)]
-pub struct Instance {
-    slug: String,
-    name: String,
-    url: String,
-}
-
-impl Instance {
-    pub fn new(slug: String, name: String, url: String) -> Self {
-        Self { slug, name, url }
-    }
-}
-
-#[derive(Debug, Getters, Default, PartialEq, Serialize, Deserialize)]
-pub struct InstancesList {
-    instances: Vec<Instance>,
-    update_date: Option<NaiveDate>,
-}
-
-impl InstancesList {
-    pub fn new(instances: Vec<Instance>, update_date: Option<NaiveDate>) -> Self {
-        Self {
-            instances,
-            update_date,
-        }
-    }
-}
+pub type InstancesList = uda_dto::uda_instance::InstancesList;
--- dto/src/uda_member.rs
+++ dto/src/uda_member.rs
@@ -1,92 +1,38 @@
 use crate::member_to_check::MemberToCheck;
-use derive_getters::Getters;
-use serde::{Deserialize, Serialize};
-use std::cmp::Ordering;
 
-/// An [UdaMember] is a participant imported from UDA.
-/// It has a few fields, which can help to manage this member - confirm them, email them, ...
-#[derive(Debug, Getters, PartialEq, Eq, Serialize, Deserialize, Clone)]
-pub struct UdaMember {
-    id: u16,
-    membership_number: Option<String>,
-    first_name: String,
-    last_name: String,
-    email: String,
-    club: Option<String>,
-    confirmed: bool,
-}
-
-impl UdaMember {
-    pub fn new(
-        id: u16,
-        membership_number: Option<String>,
-        first_name: String,
-        last_name: String,
-        email: String,
-        club: Option<String>,
-        confirmed: bool,
-    ) -> Self {
-        Self {
-            id,
-            membership_number,
-            first_name,
-            last_name,
-            email,
-            club,
-            confirmed,
-        }
-    }
-}
-
-impl PartialOrd for UdaMember {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for UdaMember {
-    fn cmp(&self, other: &Self) -> Ordering {
-        if self.last_name() != other.last_name() {
-            self.last_name().cmp(other.last_name())
-        } else if self.first_name() != other.first_name() {
-            self.first_name().cmp(other.first_name())
-        } else {
-            self.membership_num().cmp(&other.membership_num())
-        }
-    }
-}
+pub type UdaMember = uda_dto::uda_member::UdaMember;
 
 impl MemberToCheck for UdaMember {
     fn id(&self) -> Option<u16> {
-        Some(self.id)
+        Some(*self.id())
     }
 
     fn membership_num(&self) -> Option<String> {
-        self.membership_number.clone()
+        self.membership_number().clone()
     }
 
     fn identity(&self) -> Option<String> {
-        Some(format!("{} {}", self.first_name, self.last_name))
+        Some(format!("{} {}", self.first_name(), self.last_name()))
     }
 
     fn first_name(&self) -> Option<String> {
-        Some(self.first_name.clone())
+        Some(self.first_name().clone())
     }
 
     fn last_name(&self) -> Option<String> {
-        Some(self.last_name.clone())
+        Some(self.last_name().clone())
     }
 
     fn email(&self) -> Option<String> {
-        Some(self.email.clone())
+        Some(self.email().clone())
     }
 
     fn club(&self) -> Option<String> {
-        self.club.clone()
+        self.club().clone()
     }
 
     fn confirmed(&self) -> Option<bool> {
-        Some(self.confirmed)
+        Some(*self.confirmed())
     }
 }
 
